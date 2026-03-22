b04bbbeaedf7455742b7215ed58c4a1d97b0dfcd
fixpointer <>
2026-03-22 01:33:37 +0100

    register mask for libcef transparency flag finder

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 all: patcher_lib.so
 
-
+CFLAGS := -g -O3 -Wall -Wno-parentheses -masm=intel -Iproc_maps_parser/include  -shared -fpic -z defs
+CXXFLAGS := -std=c++23
 proc_maps_parser:
 	git clone https://github.com/ouadev/proc_maps_parser
 
 proc_maps_parser/build/libpmparser.a: proc_maps_parser
 	cd proc_maps_parser && $(MAKE)
 
-patcher_lib.so: patcher_lib.c proc_maps_parser/build/libpmparser.a
+patcher_lib.so: patcher_lib.cc proc_maps_parser/build/libpmparser.a
 	# link against static libpmparser, build a shared library
-	gcc -Wall -masm=intel -Iproc_maps_parser/include -o patcher_lib.so -shared -fpic -z defs patcher_lib.c -lc -l:libpmparser.a -L./proc_maps_parser/build
+	$(CXX) $(CFLAGS) $(CXXFLAGS) -o patcher_lib.so patcher_lib.cc -lc -l:libpmparser.a -L./proc_maps_parser/build
 
 
 clean:
--- patcher_lib.cc
+++ patcher_lib.cc
@@ -1,7 +1,10 @@
-#define _GNU_SOURCE
+// #define _GNU_SOURCE
 #include <link.h>
 #include <libgen.h>
+extern "C" {
 #include <pmparser.h>
+}
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,12 +12,21 @@
 #include <sys/mman.h>
 #include <stdbool.h>
 #include <dlfcn.h>
+#include <aero.h>
 
+#include <span>
+
+using std::span;
+
+struct CreateWindowRequest_part {
+    uint8_t depth;
+    x11::Window window;
+};
 
 #define nitems(x) (sizeof(x)/sizeof((x)[0]))
 
 char** get_argv(void* stack_start, void* stack_end, int* argc_out) {
-    void* cur = environ;
+    uint8_t* cur = (uint8_t*) environ;
 
     // walk backwards from the environ ptr to find the pointer to the program stack base,
     // which is before the argv and easily identifiable because it points to itself + 8
@@ -44,6 +56,42 @@ char** get_argv(void* stack_start, void* stack_end, int* argc_out) {
     return NULL;
 }
 
+/**
+ * Find a `needle` in `haystack` where the bytes match after applying needle_mask
+ */
+std::optional<span<uint8_t>> masked_memfind(span<uint8_t> needle, span<uint8_t> haystack, span<uint8_t> needle_mask) {
+    for (size_t offset = 0; offset < haystack.size() - needle.size(); offset++) {
+        for (size_t i = 0; i < needle.size(); ) {
+            if (needle.size() - i > 8 && (uintptr_t)haystack.data() & 0x7 == 0) { 
+                if (*(uint64_t*)(haystack.data() + offset + i) & *(uint64_t*)(needle_mask.data()+i)
+                != *(uint64_t*)(needle.data() + i) & *(uint64_t*)(needle_mask.data() + i) 
+                ) goto not_found;
+
+                i += 8;
+            } else {
+                if ((haystack[offset+i] & needle_mask[i]) != (needle[i] & needle_mask[i])) goto not_found;
+                i += 1;
+            }
+            
+        }
+        return std::optional(span(haystack.data()+offset, needle.size()));
+        not_found:
+    }
+    return std::optional<span<uint8_t>>{};
+}
+
+span<uint8_t> span_page_align(span<uint8_t> sp) {
+    return {
+        (uint8_t*)( (uintptr_t) sp.data() & ~0xfffull),
+        (uint8_t*)( (uintptr_t) sp.data() + sp.size() + 0xfff & ~0xfffull)
+    };
+}
+
+int mprotect_span(span<uint8_t> data, int prot) {
+    auto pa = span_page_align(data);
+    return mprotect(pa.data(), pa.size(), prot);
+}
+
 unsigned int la_version(unsigned int version) {
 
     procmaps_error_t parser_err;
@@ -52,7 +100,8 @@ unsigned int la_version(unsigned int version) {
 
     if (parser_err != PROCMAPS_SUCCESS) {
         fprintf(stderr, "[aero_patcher]: could not parse memory maps. no patch applied\n");
-        goto free_pm_and_return;
+        pmparser_free(&maps);
+        return LAV_CURRENT;
     }
 
     procmaps_struct *mem_region;
@@ -60,9 +109,10 @@ unsigned int la_version(unsigned int version) {
     char imagename_buf[256];
     size_t imagename_len;
 
-    if ((imagename_len = readlink("/proc/self/exe", imagename_buf, 256)) == -1) {
+    if ((imagename_len = readlink("/proc/self/exe", imagename_buf, 256)) == (size_t) -1) {
         fprintf(stderr, "[aero_patcher]: could not read /proc/self/exe\n");
-        goto free_pm_and_return;
+        pmparser_free(&maps);
+        return LAV_CURRENT;
     }
 
     imagename_buf[imagename_len] = 0;
@@ -78,7 +128,8 @@ unsigned int la_version(unsigned int version) {
 
     if (!main_text) {
         fprintf(stderr, "[aero_patcher]: could not determine main executable region. no patch applied\n");
-        goto free_pm_and_return;
+        pmparser_free(&maps);
+        return LAV_CURRENT;
     }
 
 
@@ -109,7 +160,7 @@ unsigned int la_version(unsigned int version) {
     };
 
     while(true) {
-        void* result = memmem(search_start, (uintptr_t) main_text->addr_end - (uintptr_t) search_start, "\x12\x12\x12\xff", 4);
+        uint8_t* result = (uint8_t*) memmem(search_start, (uintptr_t) main_text->addr_end - (uintptr_t) search_start, "\x12\x12\x12\xff", 4);
 
         if (!result) break;
 
@@ -118,7 +169,7 @@ unsigned int la_version(unsigned int version) {
 
 
         // check for match with one of the allowed prefixes
-        for (int i = 0; i < nitems(allowed_prefixes) && !prefix_valid; i++) {
+        for (unsigned int i = 0; i < nitems(allowed_prefixes) && !prefix_valid; i++) {
             uint64_t tmp = *(uint64_t*) (result-8);
             tmp &= *(uint64_t*)allowed_prefixes[i].mask;
 
@@ -128,7 +179,8 @@ unsigned int la_version(unsigned int version) {
         if (prefix_valid) {
             if (mprotect((void*)((uintptr_t)result & ~0xfffULL), ((uintptr_t) result & 0xfff) > 0x1000-4 ? 0x2000: 0x1000, PROT_WRITE | PROT_READ) == -1) {
                 perror("[aero_patcher]: mprotect failure\n");
-                goto free_pm_and_return;
+                pmparser_free(&maps);
+                return LAV_CURRENT;
             }
 
             fprintf(stderr, "[aero_patcher] found color patch pattern 121212ff @ %p\n", result);
@@ -136,7 +188,8 @@ unsigned int la_version(unsigned int version) {
 
             if (mprotect((void*)((uintptr_t)result & ~0xfffULL), ((uintptr_t) result & 0xfff) > 0x1000-4 ? 0x2000: 0x1000, PROT_EXEC | PROT_READ) == -1) {
                 perror("[aero_patcher]: mprotect failure\n");
-                goto free_pm_and_return;
+                pmparser_free(&maps);
+                return LAV_CURRENT;
             }
         }
 
@@ -146,45 +199,137 @@ unsigned int la_version(unsigned int version) {
         //fprintf(stderr, "[aero_patcher] successfully patched text offset 0x%lx\n", (uintptr_t) result - (uintptr_t)(main_text->addr_start));
         search_start = result+4;
 
-        if (search_start + 4 >= main_text->addr_end) break;
+        if ((uint8_t*) search_start + 4 >= main_text->addr_end) break;
     }
-
-    free_pm_and_return:
 
     // free function is safe even if pointer is null
     pmparser_free(&maps);
     return LAV_CURRENT;
 }
 
-__attribute__((naked)) void __patch_pattern() {
-    asm volatile(
-        "patch_pattern_start: mov eax, [r14+0x18];"
-        "test eax, eax;"
-        "jz 1f;"
-        "cmp eax, 2;"
-        "jz 2f;"
-        "xor r13d, r13d;"
-        "jmp 3f;"
-        "1: cmp dword ptr [r14], 4;"
-        "setz r13b;"
-        "jmp 3f;"
-        "2: mov r13b,1;"
-        "3: patch_pattern_end: nop"
-    );
+uint8_t patch_pattern_wincreate[] = {
+    // mov eax, [r14+0x18]
+    0x41, 0x8b, 0x46, 0x18,
+
+    // test eax, eax
+    0x85, 0xc0,
+
+    // jz 1f
+    0x74, 0x0a,
+
+    // cmp eax, 2
+    0x83, 0xf8, 0x02,
+
+    // jz 2f
+    0x74, 0x0f,
+
+    // xor r12d, r12d
+    0x45, 0x31, 0xe4,
+
+    // jmp 3f
+    0xeb, 0x0d,
+
+    // cmp dword ptr [r14], 4
+    0x41, 0x83, 0x3e, 0x04,
+
+    // setz r12b
+    0x41, 0x0f, 0x94, 0xc4,
+
+    // jmp 3f
+    0xeb, 0x03,
+
+    // mov r12b, 1
+    0x41, 0xb4, 0x01
+};
+
+// comparison masks for mod r/m byte to allow any reg either in "r" only or in "r/m", with fixed mod of 3 (immediate register, no memory access)
+const uint8_t MODRM_ANY_R_MASK = 0xc7;
+const uint8_t MODRM_ANY_M_MASK = 0xf8;
+const uint8_t MODRM_ANY_RM_MASK = 0xc0;
+
+
+uint8_t patch_pattern_wincreate_mask[] = {
+    // mov eax, [r14+0x18]
+    0xff,0xff,0xff,0xff,
+
+    // test eax, eax
+    0xff,0xff,
+
+    // jz 1f
+    0xff,0xff,
+
+    // cmp eax, 2
+    0xff,0xff,0xff,
+
+    // jz 2f
+    0xff,0xff,
+
+    // xor r12d, r12d - REX prefix, opcode, mod r/m byte
+    0xff,0xff, MODRM_ANY_RM_MASK,
+
+    // jmp 3f
+    0xff,0xff,
+
+    // cmp dword ptr [r14], 4
+    0xff,0xff,0xff,0xff,
+
+    // setz r12b - REX prefix, 2-byte opcode escape, opcode, mod r/m byte with r fixed to 0 (and r/m specifying register)
+    0xff, 0xff, 0xff, MODRM_ANY_M_MASK,
+
+    // jmp 3f
+    0xff, 0xff,
+
+    // mov r12b, 1 - REX prefix, base opcode b0 | reg, immediate
+    0xff, 0xf8, 0xff
+};
+
+
+
+/**
+ * Find the code in libcef that is responsible for handling X11 window transparency and patch it to request all windows to be transparent
+ * 
+ * @returns if the patch has been found and applied successfully
+ */
+bool apply_libcef_transparency_patch(span<uint8_t> libcef_text, void* libcef_baseaddr) {
+    if (auto res = masked_memfind(patch_pattern_wincreate, libcef_text, patch_pattern_wincreate_mask )) {
+        span<uint8_t> patch_destination = *res;
+
+
+        // get the register number used for the register holding the transparency flag
+        uint8_t transparency_flag_reg = patch_destination[0xf] & 7;
+
+        fprintf(stderr, "[aero_patcher] reg r%d\n", 8+transparency_flag_reg);
+
+        if (mprotect_span(patch_destination,PROT_WRITE | PROT_READ)
+            == -1) {
+            perror("[aero_patcher]: mprotect failure");
+            return false;
+        }
+
+
+        fprintf(stderr, "[aero_patcher] found x11 transparency flag handling code @ %p (offset %lx). will apply patch of length %ld\n", patch_destination.data(), patch_destination.data() - (uint8_t*)libcef_baseaddr, patch_destination.size());
+
+
+        memset(patch_destination.data(), '\x90', patch_destination.size());
+
+
+        // mov <reg>, 1
+        uint8_t patch[] = {0x41, (uint8_t)( 0xb0 | transparency_flag_reg), 0x01};
+        memcpy(patch_destination.data(),patch, sizeof(patch));
+
+
+
+        if (mprotect_span(patch_destination,  PROT_READ | PROT_EXEC)
+            == -1) {
+            perror("[aero_patcher]: mprotect failure");
+            return false;
+        }
+        return true;
+
+    } else { return false;}
+
+
 }
-
-__attribute__((naked)) void __patch() {
-    asm volatile(
-        "patch_start: mov r13d, 1; patch_end: nop"
-    );
-}
-
-void patch_pattern_start(void);
-void patch_pattern_end(void);
-
-void patch_start(void);
-void patch_end(void);
-
 
 unsigned int la_objopen(struct link_map* map, Lmid_t lmid, uintptr_t *cookie) {
     char* path = strdup(map->l_name);
@@ -200,7 +345,8 @@ unsigned int la_objopen(struct link_map* map, Lmid_t lmid, uintptr_t *cookie) {
 
     if (parser_err != PROCMAPS_SUCCESS) {
         fprintf(stderr, "[aero_patcher]: could not parse memory maps. no patch applied\n");
-        goto free_pm_and_return;
+        pmparser_free(&maps);
+        return LAV_CURRENT;
     }
 
     procmaps_struct *mem_region;
@@ -219,13 +365,11 @@ unsigned int la_objopen(struct link_map* map, Lmid_t lmid, uintptr_t *cookie) {
 
     if (!libcef_text) {
         fprintf(stderr, "[aero_patcher] could not find libcef text\n");
-        goto free_pm_and_return;
+        pmparser_free(&maps);
+        return LAV_CURRENT;
     }
 
-    void* patch_destination = memmem(libcef_text->addr_start, libcef_text->length,
-                                     patch_pattern_start, (uintptr_t) patch_pattern_end - (uintptr_t) patch_pattern_start);
-
-    if (!patch_destination) {
+    if (!apply_libcef_transparency_patch(span<uint8_t>((uint8_t*)libcef_text->addr_start, (uint8_t*) libcef_text->addr_end), libcef_base->addr_start)) {
         fprintf(stderr, "[aero_patcher] could not find libcef patch pattern. This may happen when Spotify updates their libcef");
         fprintf(stderr, "[aero_patcher] please file an issue on GitHub and provide the following information: \n");
 
@@ -242,7 +386,7 @@ unsigned int la_objopen(struct link_map* map, Lmid_t lmid, uintptr_t *cookie) {
         }
 
         if (rodata) {
-            cef_version_info = memmem(rodata->addr_start, rodata->length,"+chromium-",10);
+            cef_version_info = (char*)memmem(rodata->addr_start, rodata->length,"+chromium-",10);
 
             if (cef_version_info) {
                 while(*cef_version_info) cef_version_info--;
@@ -257,42 +401,12 @@ unsigned int la_objopen(struct link_map* map, Lmid_t lmid, uintptr_t *cookie) {
 
         fprintf(stderr, "[aero_patcher] if possible, also upload your libcef.so (found under %s) somewhere and provide a link in the issue\n", map->l_name);
 
-        goto free_pm_and_return;
+        pmparser_free(&maps);
+        return LAV_CURRENT;
     }
 
-    if (mprotect(
-            (void*)((uintptr_t)patch_destination & ~0xfffULL),
-            ((uintptr_t)patch_destination & 0xfff) > 0x1000-4 ? 0x2000: 0x1000,
-            PROT_WRITE | PROT_READ)
-        == -1) {
-        perror("[aero_patcher]: mprotect failure");
-        goto free_pm_and_return;
-    }
-
-    size_t patch_len = (uintptr_t) patch_end - (uintptr_t) patch_start;
-    size_t patch_pattern_len = (uintptr_t) patch_pattern_end - (uintptr_t) patch_pattern_start;
-
-
-    fprintf(stderr, "[aero_patcher] found x11 transparency flag handling code @ %p (offset %lx). will apply patch of length %ld\n", patch_destination, patch_destination - libcef_base->addr_start, patch_pattern_len);
-
-
-    memset(patch_destination, '\x90', patch_pattern_len);
-    memcpy(patch_destination,(void*)patch_start, patch_len);
-
-
-
-    if (mprotect(
-            (void*)((uintptr_t)patch_destination & ~0xfffULL),
-            ((uintptr_t)patch_destination & 0xfff) > 0x1000-4 ? 0x2000: 0x1000,
-            PROT_EXEC | PROT_READ)
-        == -1) {
-        perror("[aero_patcher]: mprotect failure");
-        goto free_pm_and_return;
-    }
 
     fprintf(stderr, "[aero_patcher] have successfully patched libcef\n");
-
-    free_pm_and_return:
 
     // free function is safe even if pointer is null
     pmparser_free(&maps);
